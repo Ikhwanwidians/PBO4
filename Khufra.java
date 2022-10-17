a5bad8ccadf384c9177d14fcc3c802af2aa7bfe9
Ikhwanwidians <ikhwanwidiansyah411@gmail.com>
2022-10-17 16:27:48 +0700

    Child class

---
+++ Khufra.java
@@ -0,0 +1,13 @@
+
+package Sesi4Pbo;
+
+public class Khufra extends HeroMobileLegends {
+    public void katakataHero(){
+        super.katakataHero();
+        System.out.println("Hero Khufra : Semua akan tunduk pada kekuasaanku");
+    }
+    public static void main(String[] args) {
+        Khufra Tanker = new Khufra();
+        Tanker.katakataHero();
+    }
+}
