c86164c90f80f972f03b5b309c0680863b1978b0
Ikhwanwidians <ikhwanwidiansyah411@gmail.com>
2022-10-17 16:28:42 +0700

    child class

---
+++ Leomord.java
@@ -0,0 +1,13 @@
+
+package Sesi4Pbo;
+
+public class Leomord extends HeroMobileLegends {
+    public void katakataHero(){
+        super.katakataHero();
+        System.out.println("Hero Leomord : Keberanian dan kesetiaan dalam tubuh yang sudah mati");
+    }
+    public static void main(String[] args) {
+        Leomord Fighter = new Leomord();
+        Fighter.katakataHero();
+    }
+}
