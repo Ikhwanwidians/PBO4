2b201669fb2d0fd8ad7a6b2554e260469f288304
Ikhwanwidians <ikhwanwidiansyah411@gmail.com>
2022-10-17 16:30:05 +0700

    child class

---
+++ Nathan.java
@@ -0,0 +1,13 @@
+
+package Sesi4Pbo;
+
+public class Nathan extends HeroMobileLegends {
+    public void katakataHero(){
+        super.katakataHero();
+        System.out.println("Hero Nathan : Dalam keputusasaan aku melihat harapan yang paling terang");
+    }
+    public static void main(String[] args) {
+        Nathan Marksman = new Nathan();
+        Marksman.katakataHero();
+    }
+}
